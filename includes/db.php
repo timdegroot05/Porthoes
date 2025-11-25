eb5e1657214f0c735e0ca16e33a44792fe8b12c1
Igonevanderkley <117640298+Igonevanderkley@users.noreply.github.com>
2025-11-25 11:57:30 +0100

    db connectie

---
+++ includes/db.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "ActiviteitenDB";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("L database connectie!: " . $conn->connect_error);
+} else {
+    echo "W database connectie!";
+}
---
+++ public/index.php
@@ -0,0 +1,39 @@
+<?php
+include 'includes/db.php';
+
+// Haal alle activiteiten op
+$sql = "SELECT * FROM Activiteiten";
+$result = $conn->query($sql);
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Activiteiten Overzicht</title>
+</head>
+
+<body>
+
+    <h1>Activiteiten</h1>
+
+    <?php if ($result->num_rows > 0): ?>
+        <ul>
+            <?php while ($row = $result->fetch_assoc()): ?>
+                <li>
+                    <strong><?= $row['naam'] ?></strong><br>
+                    <?= $row['beschrijving'] ?><br>
+                    Max deelnemers: <?= $row['max_deelnemers'] ?><br>
+                    Prijs: €<?= $row['prijs'] ?><br>
+                    <hr>
+                </li>
+            <?php endwhile; ?>
+        </ul>
+
+    <?php else: ?>
+        <p>Geen activiteiten gevonden.</p>
+    <?php endif; ?>
+
+</body>
+
+</html>
